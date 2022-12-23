0223e77dbf1bdc5c96d0ae8c04b15b77ff593e5f
Andi Barg <47257059+andibarg@users.noreply.github.com>
2022-12-23 09:15:48 +0100

    Update __init__.py

--- zospy/analyses/__init__.py
+++ zospy/analyses/__init__.py
@@ -2,6 +2,6 @@ from .base import AnalysisResult, AttrDict
 from .mtf import *
 from .psf import *
 from .reports import *
-from .raytrace import *
+from .raysandspots import *
 from .surface import *
 from .wavefront import *
