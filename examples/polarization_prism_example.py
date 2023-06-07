444bf5f0df198d6143d6389f397881125c49a0b1
chaasjes <chaasjes@lumc.nl>
2023-06-07 16:04:11 +0200

    Fix errors in example

--- examples/polarization_prism_example.py
+++ examples/polarization_prism_example.py
@@ -1,7 +1,4 @@
-"""
-In standalone runs and plots polarization 
-analyses in Prism using total internal reflection.
-"""
+"""In standalone runs and plots polarization analyses in Prism using total internal reflection."""
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -34,24 +31,28 @@ oss.load(test_file)
 
 # Polarization
 print("Calculating Transmission ...\n")
-return_transmission = zp.analyses.transmission(oss, jx=jx, jy=jy, x_phase=x_phase, y_phase=y_phase, sampling="64x64")
+return_transmission = zp.analyses.polarization.transmission(
+    oss, jx=jx, jy=jy, x_phase=x_phase, y_phase=y_phase, sampling="64x64"
+)
 for key, value in return_transmission["Data"].items():
     if key != "Header":
         print("%s  \t%.2f%%" % (key, value["Total Transmission"] * 100))
 
 # Get polarization map
 print("\nGenerating Polarization Pupil Map ...")
-ret = zp.analyses.polarization_pupil_map(oss, jx=jx, jy=jy, x_phase=x_phase, y_phase=y_phase, sampling="17x17")
+ret = zp.analyses.polarization.polarization_pupil_map(
+    oss, jx=jx, jy=jy, x_phase=x_phase, y_phase=y_phase, sampling="17x17"
+)
 df = ret["Data"]["Table"]
 
 # Plot map
-xylen = len(np.unique(df["Px"]))
+xy_length = len(np.unique(df["Px"]))
 for ii in range(len(df)):
     # E-field coordinates
     phi = np.linspace(0, 2 * np.pi) - np.pi / 3
-    Ex = np.real(df["Ex"][ii] * np.exp(1j * phi)) / xylen + df["Px"].iloc[ii]
+    Ex = np.real(df["Ex"][ii] * np.exp(1j * phi)) / xy_length + df["Px"].iloc[ii]
     Ey = (
-        np.real(df["Ey"].iloc[ii] * np.exp(1j * phi + 1j * df["Phase(Deg)"].iloc[ii] * np.pi / 180)) / xylen
+        np.real(df["Ey"].iloc[ii] * np.exp(1j * phi + 1j * df["Phase(Deg)"].iloc[ii] * np.pi / 180)) / xy_length
         + df["Py"].iloc[ii]
     )
 
