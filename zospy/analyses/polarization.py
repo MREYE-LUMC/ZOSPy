270a66f30cdc361753ce257b0b57007908eacfb1
Luc van Vught <l.van_vught@lumc.nl>
2023-07-25 12:57:19 +0200

    Updated regex to account for more potential cases

--- zospy/analyses/polarization.py
+++ zospy/analyses/polarization.py
@@ -20,7 +20,8 @@ from zospy.zpcore import OpticStudioSystem
 
 def _get_number_field(name: str, text: str) -> str:
     return re.search(
-        rf"{re.escape(name)}\s*:\s*([-+]?\d+{re.escape(_config.DECIMAL_POINT)}?\d*(?:[Ee][-+]?\d+)?)", text
+        rf"{re.escape(name)}\s*:\s*([-+]?(\d+({re.escape(_config.DECIMAL_POINT)}\d*)?|{re.escape(_config.DECIMAL_POINT)}\d+)(?:[Ee][-+]?\d+)?)",
+        text,
     ).group(1)
 
 
