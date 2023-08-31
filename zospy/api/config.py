3e66be3b844a97d2f308f5c9d4237b5fcddee344
chaasjes <chaasjes@lumc.nl>
2023-08-31 14:18:19 +0200

    Skip config when run by readthedocs

--- zospy/api/config.py
+++ zospy/api/config.py
@@ -1,8 +1,11 @@
 import locale
+import os
 
-# Get decimal point used by ZOS
-loc = locale.getlocale()  # get and save current locale
-locale.setlocale(locale.LC_ALL, "")
-DECIMAL_POINT = locale.localeconv()["decimal_point"]
-THOUSANDS_SEPARATOR = locale.localeconv()["thousands_sep"]
-locale.setlocale(locale.LC_ALL, loc)  # restore saved locale
+# Do not run this code when building documentation
+if not os.environ.get("READTHEDOCS"):
+    # Get decimal point used by ZOS
+    loc = locale.getlocale()  # get and save current locale
+    locale.setlocale(locale.LC_ALL, "")
+    DECIMAL_POINT = locale.localeconv()["decimal_point"]
+    THOUSANDS_SEPARATOR = locale.localeconv()["thousands_sep"]
+    locale.setlocale(locale.LC_ALL, loc)  # restore saved locale
