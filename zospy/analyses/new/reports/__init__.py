5105fb2f7de8a3219ba417d16d5780b8e030f933
crnh <30109443+crnh@users.noreply.github.com>
2024-11-27 11:21:49 +0100

    Add new-style analysis and tests for system data

--- zospy/analyses/new/reports/__init__.py
+++ zospy/analyses/new/reports/__init__.py
@@ -1,5 +1,6 @@
 """OpticStudio analyses from the Reports category."""
 
 from zospy.analyses.new.reports.surface_data import SurfaceData
+from zospy.analyses.new.reports.system_data import SystemData
 
-__all__ = ("SurfaceData",)
+__all__ = ("SurfaceData", "SystemData")
