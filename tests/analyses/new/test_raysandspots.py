c2aed6f04310cc29b09aa2a02fb8ff52cc9003d8
crnh <30109443+crnh@users.noreply.github.com>
2024-12-12 15:57:19 +0100

    New-style Rays and Spots analyses (#92)
    * Add new-style Ray Fan analysis and tests
    
    * Add dataframe conversion to Ray Fan result
    
    * Handle 2D field coordinates

--- tests/analyses/new/test_raysandspots.py
+++ tests/analyses/new/test_raysandspots.py
@@ -1,4 +1,4 @@
-from zospy.analyses.new.raysandspots import SingleRayTrace
+from zospy.analyses.new.raysandspots import RayFan, SingleRayTrace
 
 
 class TestSingleRayTrace:
@@ -8,4 +8,14 @@ class TestSingleRayTrace:
 
     def test_to_json(self, simple_system):
         result = SingleRayTrace().run(simple_system)
+        assert result.from_json(result.to_json()).to_json() == result.to_json()
+
+
+class TestRayFan:
+    def test_can_run(self, simple_system):
+        result = RayFan().run(simple_system)
+        assert result.data is not None
+
+    def test_to_json(self, simple_system):
+        result = RayFan().run(simple_system)
         assert result.from_json(result.to_json()).to_json() == result.to_json()
