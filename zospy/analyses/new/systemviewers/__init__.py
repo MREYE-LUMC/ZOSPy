3a4c3d3ef3f020b1463e2d2b7bc8c6d68de39382
crnh <30109443+crnh@users.noreply.github.com>
2024-12-12 15:49:45 +0100

    Add basic NSC shaded model viewer

--- zospy/analyses/new/systemviewers/__init__.py
+++ zospy/analyses/new/systemviewers/__init__.py
@@ -1,9 +1,20 @@
 """OpticStudio system viewers."""
 
 from zospy.analyses.new.systemviewers.cross_section import CrossSection, CrossSectionSettings
+from zospy.analyses.new.systemviewers.nsc_3d_layout import NSC3DLayout, NSC3DLayoutSettings
+from zospy.analyses.new.systemviewers.nsc_shaded_model import NSCShadedModel, NSCShadedModelSettings
 from zospy.analyses.new.systemviewers.shaded_model import ShadedModel, ShadedModelSettings
 from zospy.analyses.new.systemviewers.viewer_3d import Viewer3D, Viewer3DSettings
-from zospy.analyses.new.systemviewers.nsc_3d_layout import NSC3DLayout, NSC3DLayoutSettings
 
-__all__ = ("CrossSection", "CrossSectionSettings", "Viewer3D", "Viewer3DSettings", "ShadedModel",
-           "ShadedModelSettings", "NSC3DLayout", "NSC3DLayoutSettings")
+__all__ = (
+    "CrossSection",
+    "CrossSectionSettings",
+    "Viewer3D",
+    "Viewer3DSettings",
+    "ShadedModel",
+    "ShadedModelSettings",
+    "NSC3DLayout",
+    "NSC3DLayoutSettings",
+    "NSCShadedModel",
+    "NSCShadedModelSettings",
+)
