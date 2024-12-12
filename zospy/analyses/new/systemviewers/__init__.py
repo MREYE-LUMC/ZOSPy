a284837aa08f73536755a9e8ff9b2b0f3493c71d
crnh <30109443+crnh@users.noreply.github.com>
2024-12-12 13:33:56 +0100

    Add support for 3D viewer

--- zospy/analyses/new/systemviewers/__init__.py
+++ zospy/analyses/new/systemviewers/__init__.py
@@ -1,5 +1,6 @@
 """OpticStudio system viewers."""
 
 from zospy.analyses.new.systemviewers.cross_section import CrossSection, CrossSectionSettings
+from zospy.analyses.new.systemviewers.viewer_3d import Viewer3D, Viewer3DSettings
 
-__all__ = ("CrossSection", "CrossSectionSettings")
+__all__ = ("CrossSection", "CrossSectionSettings", "Viewer3D", "Viewer3DSettings")
