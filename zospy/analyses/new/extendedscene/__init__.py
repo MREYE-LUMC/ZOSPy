296b195b4850e16366a8c2a5faf7243faf633c0c
lvanvught <l.van_vught@lumc.nl>
2025-01-03 17:24:32 +0100

    Added new version of extended scene analysis

---
+++ zospy/analyses/new/extendedscene/__init__.py
@@ -0,0 +1,5 @@
+"""OpticStudio analyses from the Extended Scene category."""
+
+from zospy.analyses.new.extendedscene.geometric_image_analysis import GeometricImageAnalysis, GeometricImageAnalysisSettings
+
+__all__ = ("GeometricImageAnalysis", "GeometricImageAnalysisSettings")
