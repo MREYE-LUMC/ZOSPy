68964764fa0c3ceee6374d4b641c7cb6ce007b2f
lvanvught <l.van_vught@lumc.nl>
2025-03-05 15:07:05 +0100

    Undo accidental change in config.py

--- tests/config.py
+++ tests/config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 # Reference version of OpticStudio, with which all analysis outputs are compared
-REFERENCE_VERSION: str = "24.1.3"
+REFERENCE_VERSION: str = "23.1.0"
 
 # Folder with configuration files for tests of *_fromcfg analyses
 CONFIG_DATA_FOLDER: Path = Path("tests/data/config")
