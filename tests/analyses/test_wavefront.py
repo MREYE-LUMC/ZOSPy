23cdc0ed7d710f4ce5878b88947ffe1ea52cb52a
crnh <30109443+crnh@users.noreply.github.com>
2025-11-25 15:31:00 +0100

    Add Zernike coefficients vs fields analysis (#187)
    * Add Zernike Coefficients VS Field analysis
    
    * Parse lines with only whitespace as text
    
    * Add tests and reference data for Zernike Coefficients vs. Field analysis

--- tests/analyses/test_wavefront.py
+++ tests/analyses/test_wavefront.py
@@ -1,10 +1,17 @@
 from __future__ import annotations
 
+from contextlib import nullcontext as does_not_raise
+
 import pytest
 from pandas.testing import assert_frame_equal
+from pydantic_core import ValidationError
 
 from tests.helpers import assert_dataclass_equal
 from zospy.analyses.wavefront import WavefrontMap, ZernikeStandardCoefficients
+from zospy.analyses.wavefront.zernike_coefficients_vs_field import (
+    ZernikeCoefficientsVsField,
+    ZernikeCoefficientsVsFieldSettings,
+)
 
 
 class TestWavefrontMap:
@@ -99,3 +106,109 @@ class TestZernikeStandardCoefficients:
             result.data,
             reference_data.data,
         )
+
+
+class TestZernikeCoefficientsVsField:
+    @pytest.mark.parametrize(
+        "coefficients,expectation",
+        [
+            ("1-15", does_not_raise()),
+            ("1,3,5,7,9", does_not_raise()),
+            ("1-5,10-15", does_not_raise()),
+            ("1,2-8,10,12-16", does_not_raise()),
+            ("a-b", pytest.raises(ValidationError)),
+            ("1-kaas", pytest.raises(ValidationError)),
+            ("", pytest.raises(ValidationError)),
+        ],
+    )
+    def test_settings_validates_coefficients(self, coefficients, expectation):
+        with expectation:
+            settings = ZernikeCoefficientsVsFieldSettings(coefficients=coefficients)
+
+            assert settings.coefficients == coefficients
+
+    def test_can_run(self, simple_system):
+        result = ZernikeCoefficientsVsField().run(simple_system)
+        assert result is not None
+
+    def test_to_json(self, simple_system):
+        result = ZernikeCoefficientsVsField().run(simple_system)
+        assert result.from_json(result.to_json()).to_json() == result.to_json()
+
+    @pytest.mark.parametrize(
+        "coefficients,coefficients_type,field_scan,field_density,sampling,obscuration,minimum_plot_scale,maximum_plot_scale",
+        [
+            ("1-8", "Standard", "+y", 20, "64x64", 0.5, 0, 0),
+            ("2", "Standard", "-y", 10, "32x32", 0.5, 0, 0),
+            ("1,3,5", "Standard", "+x", 5, "32x32", 0.5, 0, 1),
+            ("1-15", "Fringe", "-x", 15, "128x128", 0.5, 1, 5),
+            ("1-2,3,5-7", "Annular", "Plus_Y", 1, "32x32", 0.5, 0, 0),
+            ("1-2,3,5-7", "Annular", "Minus_Y", 1, "32x32", 0.1, 0, 0),
+            ("1-2,3,5-7", "Standard", "Plus_X", 1, "32x32", 0.5, 1, 5),
+            ("1-2,3,5-7", "Standard", "Minus_X", 1, "32x32", 0.5, 0, 0),
+        ],
+    )
+    def test_zernike_coefficients_vs_field_returns_correct_result(
+        self,
+        coefficients,
+        coefficients_type,
+        field_scan,
+        field_density,
+        sampling,
+        obscuration,
+        minimum_plot_scale,
+        maximum_plot_scale,
+        simple_system,
+        expected_data,
+    ):
+        result = ZernikeCoefficientsVsField(
+            coefficients=coefficients,
+            coefficients_type=coefficients_type,
+            field_scan=field_scan,
+            field_density=field_density,
+            sampling=sampling,
+            obscuration=obscuration,
+            minimum_plot_scale=minimum_plot_scale,
+            maximum_plot_scale=maximum_plot_scale,
+        ).run(simple_system)
+
+        assert_frame_equal(result.data, expected_data.data)
+
+    @pytest.mark.parametrize(
+        "coefficients,coefficients_type,field_scan,field_density,sampling,obscuration,minimum_plot_scale,maximum_plot_scale",
+        [
+            ("1-8", "Standard", "+y", 20, "64x64", 0.5, 0, 0),
+            ("2", "Standard", "-y", 10, "32x32", 0.5, 0, 0),
+            ("1,3,5", "Standard", "+x", 5, "32x32", 0.5, 0, 1),
+            ("1-15", "Fringe", "-x", 15, "128x128", 0.5, 1, 5),
+            ("1-2,3,5-7", "Annular", "Plus_Y", 1, "32x32", 0.5, 0, 0),
+            ("1-2,3,5-7", "Annular", "Minus_Y", 1, "32x32", 0.1, 0, 0),
+            ("1-2,3,5-7", "Standard", "Plus_X", 1, "32x32", 0.5, 1, 5),
+            ("1-2,3,5-7", "Standard", "Minus_X", 1, "32x32", 0.5, 0, 0),
+        ],
+    )
+    def test_zernike_coefficients_vs_field_matches_reference_data(
+        self,
+        coefficients,
+        coefficients_type,
+        field_scan,
+        field_density,
+        sampling,
+        obscuration,
+        minimum_plot_scale,
+        maximum_plot_scale,
+        simple_system,
+        reference_data,
+    ):
+        result = ZernikeCoefficientsVsField(
+            coefficients=coefficients,
+            coefficients_type=coefficients_type,
+            field_scan=field_scan,
+            field_density=field_density,
+            sampling=sampling,
+            obscuration=obscuration,
+            minimum_plot_scale=minimum_plot_scale,
+            maximum_plot_scale=maximum_plot_scale,
+        ).run(simple_system)
+
+        assert_frame_equal(result.data, reference_data.data)
--- zospy/analyses/wavefront/__init__.py
+++ zospy/analyses/wavefront/__init__.py
@@ -3,9 +3,20 @@
 from __future__ import annotations
 
 from zospy.analyses.wavefront.wavefront_map import WavefrontMap, WavefrontMapSettings
+from zospy.analyses.wavefront.zernike_coefficients_vs_field import (
+    ZernikeCoefficientsVsField,
+    ZernikeCoefficientsVsFieldSettings,
+)
 from zospy.analyses.wavefront.zernike_standard_coefficients import (
     ZernikeStandardCoefficients,
     ZernikeStandardCoefficientsSettings,
 )
 
-__all__ = ("WavefrontMap", "WavefrontMapSettings", "ZernikeStandardCoefficients", "ZernikeStandardCoefficientsSettings")
+__all__ = (
+    "WavefrontMap",
+    "WavefrontMapSettings",
+    "ZernikeCoefficientsVsField",
+    "ZernikeCoefficientsVsFieldSettings",
+    "ZernikeStandardCoefficients",
+    "ZernikeStandardCoefficientsSettings",
+)
